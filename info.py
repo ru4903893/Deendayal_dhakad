f19145735a8e35adcaf7fb605839c8fec848a975
ru4903893 <154866396+ru4903893@users.noreply.github.com>
2025-10-25 16:15:35 +0400

    Update info.py

--- info.py
+++ info.py
@@ -18,8 +18,8 @@ def is_enabled(value, default):
 # Bot Information Configuration
 # ============================
 SESSION = environ.get('SESSION', 'Deendayal_search')
-API_ID = int(environ.get('API_ID', '15481135'))
-API_HASH = environ.get('API_HASH', '2f1bc8786f6f8d6902bafd8896d6aa80')
+API_ID = int(environ.get('API_ID', '26107399'))
+API_HASH = environ.get('API_HASH', 'e10525d8ad0189f8bf7a82a32f538d12')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # ============================
