04fe95b58781e27a55c93557679f60d09547f113
ru4903893 <154866396+ru4903893@users.noreply.github.com>
2025-11-26 11:27:16 +0400

    Update DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK URL

--- info.py
+++ info.py
@@ -96,7 +96,7 @@ IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/movie_search_group1')
 CHNL_LNK = environ.get('CHNL_LNK', '')
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Extra_Ordinary_boy')
-DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK = environ.get('DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK', 'https://t.me/movie_search_group1')
+DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK = environ.get('DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK', 'https://t.me/+JYcBHgSBaNYxYjdl')
 OWNERID = int(os.environ.get('OWNERID', '7655321383'))  # Replace with the actual admin ID
 
 # ============================
